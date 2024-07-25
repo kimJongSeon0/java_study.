cdbf0876ca74bd61a383ef305967a409ecea8286
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-25 18:52:46 +0900

    aa

---
+++ Extends.java
@@ -0,0 +1,41 @@
+class Pet{
+    private String name;
+
+    Pet(String name){
+        this.name = name;    
+    }
+    Pet(){
+        this("");
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void eating(){
+        System.out.println(name + "가 사료를 먹는중!");
+    }
+}
+
+class MyDog extends Pet{
+
+    @Override   // 메서드의 재정의
+    public void eating(){
+        System.out.println(getName() + "가 사료를 먹는중!");
+    }
+
+    MyDog(String name){     // 자식 class
+        super(name);    // 부모 calss 생성자
+    }
+}
+public class Extends {
+    public static void main(String[] args) {
+        Pet pet = new Pet("토리");
+        MyDog dog = new MyDog("고도리");
+        
+        System.out.println(dog.getName());
+        dog.eating();
+        pet.eating();
+    }
+}
---
+++ Overloading.java
@@ -0,0 +1,20 @@
+
+class Loading {
+
+    public int getInt(int a) {
+        return 1;
+    }
+
+    public int getInt(double b) {
+        return (int)b;
+    }
+}
+
+public class Overloading {
+
+    public static void main(String[] args) {
+        Loading load = new Loading();
+        System.out.println(load.getInt(10));
+        System.out.println(load.getInt(50,55));
+    }
+}
