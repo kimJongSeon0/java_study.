95e81e839672ad10d512a755a91aebfb1cd9fc3f
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-26 16:03:54 +0900

    aa

--- day_2024_07_26/LottoMain.java
+++ day_2024_07_26/LottoMain.java
@@ -3,19 +3,50 @@ package day_2024_07_26;
 import java.util.Arrays;
 
 public class LottoMain {
-    public static void main(String[] args) {
-        final int LOTTO_COUNT = 6;
+	public static void main(String[] args) {
 
-        int[] arrLotto = new int[LOTTO_COUNT];
+		int[] intArr = new int[3];
+		
+		intArr[0] = 0;
+		intArr[1] = 1;
+		intArr[2] = 2;
+		
+		System.out.println(Arrays.toString(intArr));
+		
+		int[] intArr2 = {0,1,2};
+		System.out.println(Arrays.toString(intArr2));
+		
+		String[] season = {"spring","summer","fall","winter"};
+		String[] season2 = {new String("spring"),"summer","fall","winter"};
+		
+		System.out.println(Arrays.toString(season));
+		System.out.println(Arrays.toString(season2));
+		
+		
+		for(int i = 0; i < season.length; i++) {
+			System.out.println(season[i]);
+		}
+		
+		
+		final int LOTTO_COUNT = 6;
 
-        for(int i = 0; i < arrLotto.length; i++){
-            arrLotto[i] = (int)(Math.random() * 45) + 1;
-            for(int j = 0; j < i; j++){
-                if(arrLotto[j] == arrLotto[i])
-                i--;
-                break;
-            }
-        }
-        System.out.println(Arrays.toString(arrLotto));
-    }
+		int[] arrLotto = new int[LOTTO_COUNT];
+
+		for (int i = 0; i < arrLotto.length; i++) {
+			arrLotto[i] = (int) (Math.random() * 45) + 1;
+			for (int j = 0; j < i; j++) {
+				if (arrLotto[j] == arrLotto[i])
+				i--;
+				break;
+			}
+		}
+
+		System.out.println(Arrays.toString(arrLotto));
+
+		// 츌력
+//    	for(int i = 0; i < arrLotto.length; i++) {
+//    	System.out.print(arrLotto[i] + " ");
+//    	
+//    	}
+	}
 }
