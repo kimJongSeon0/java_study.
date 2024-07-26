b7ba0479bfd53928cbe576a5df3305295ae8e46b
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-26 17:51:08 +0900

    aa

--- day_2024_07_26/Random.java
+++ day_2024_07_26/Random.java
@@ -1,23 +1,18 @@
 package day_2024_07_26;
 
-import java.util.Arrays;
-
 public class Random {
 
     public static void main(String[] args) {
         // final int RANDOM_COUNT = 5;
 
-        int[] intRandom = {1,2,3,4,5};
-
+        int[] intRandom = {1, 5, 3, 9, 2};
+        int max = intRandom[0];
         for (int i = 0; i < intRandom.length; i++) {
             intRandom[i] = (int) (Math.random() * 5) + 1;
-            for (int j = 0; j < i; j++) {
-                if (intRandom[i] >= intRandom[j]) {
-                    break;
+                if (intRandom[i] > max) {
+                    max = intRandom[i];
                 }
             }
-            System.out.println(Arrays.toString(intRandom));
+            System.out.println(max);
         }
-
     }
-}
--- day_2024_07_26/StringChar.java
+++ day_2024_07_26/StringChar.java
@@ -3,7 +3,8 @@ package day_2024_07_26;
 public class StringChar {
 
 	public static void main(String[] args) {
-
+        Character[] ch = new Character[26];
+        
 
 	}
 
