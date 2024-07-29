2ca2e4f09dc7e9cebd63584402ccb86a1431be72
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-29 15:22:53 +0900

    aa

--- day_2024_07_29/BysubessManMain.java
+++ day_2024_07_29/BysubessManMain.java
@@ -1,48 +1,50 @@
 package day_2024_07_29;
 
 class Man {
+	String name;
 
-    String name;
+	public Man() {
 
-    public Man() {
-    }
+	}
 
-    public Man(String name) {
-        this.name = name;
-    }
+	public Man(String name) {
+		this.name = name;
+	}
 
-    public void tellYourName() {
-        System.out.println("My name is " + name);
-    }
+	public void tellYourName() {
+		System.out.println("My name is " + name);
+	}
 }
 
 class BusinessMan extends Man {
+	String company;
+	String position;
 
-    String company;
-    String position;
-
-    public BusinessMan(String name, String company, String position) {
-        //this.name = name;
+	public BusinessMan(String name, String company, String position) {
+		// 부모의 생성자를 호출함
+		
+		//this.name = name;
 		//super.name = name;
 		super(name);
 		
 		this.company = company;
-        this.position = position;
-    }
+		this.position = position;
+	}
 
-    public void tellYourInfo() {
-        System.out.println("My company is " + company);
-        System.out.println("My position is " + position);
-        tellYourName();
-    }
+	public void tellYourInfo() {
+		System.out.println("My company is " + company);
+		System.out.println("My position is " + position);
+		tellYourName();
+		//super.tellYourName();
+	}
 }
 
 public class BysubessManMain {
 
-    public static void main(String[] args) {
-        BusinessMan businessMan = new BusinessMan("홍길동","홍길동 컴퍼니", "staff");
-        businessMan.tellYourInfo();
+	public static void main(String[] args) {
+		BusinessMan businessMan = new BusinessMan("홍길동","홍길동 컴퍼니", "staff");
+		businessMan.tellYourInfo();
 
-    }
+	}
 
 }
---
+++ day_2024_07_29/SuperSubCon.java
@@ -0,0 +1,22 @@
+package day_2024_07_29;
+
+class SuperCLS{
+	public SuperCLS() {
+		System.out.println("I'm Super Class");
+	}
+}
+
+class SubCLS extends SuperCLS{
+	public SubCLS(){
+		System.out.println("I'm Sub Class");
+	}
+}
+
+public class SuperSubCon {
+
+	public static void main(String[] args) {
+		new SubCLS();
+
+	}
+
+}
