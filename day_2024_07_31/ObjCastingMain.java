dd93222517a3c8ece677f4939eb12ddf0fa4a30e
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-31 11:02:27 +0900

    aa

--- day_2024_07_31/ObjCastingMain.java
+++ day_2024_07_31/ObjCastingMain.java
@@ -12,16 +12,34 @@ class B extends A {
 	}
 }
 
+class C extends B {
+	public void ccc() {
+		System.out.println("ccc 호출");
+	}
+}
+
 public class ObjCastingMain {
 
 	public static void main(String[] args) {
-		A a = /*(A)*/ new B();	//	Polymorphism(다형성) 형변환을 안 해줘도 자동 형변환이 됨.
-		
-		
-		
-		B b = (B) a;	//	자식=(자식)부모(에러)/형변환 
-		
-		b.bbb();
+		A a = /* (A) */ new B(); // Polymorphism(다형성) 형변환을 안 해줘도 자동 형변환이 됨.
+
+		if (a instanceof B) { // a주소 찾아가서 B가 있으면 true 없으면 false // 형변환 가능한가 묻는 것.
+			B b = (B) a; // 자식=(자식)부모/형변환
+			b.bbb();
+		}
+
+		a = new C(); // Polymorphism(다형성)
+		if (a instanceof B) {
+			System.out.println("트루입니다.");
+		}
+
+		if (a instanceof A) {
+			System.out.println("트루입니다.");
+		}
+
+		if (a instanceof C) {
+			System.out.println("트루입니다.");
+		}
 	}
 
 }
