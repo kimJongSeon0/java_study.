ab4d1232521f23230c7fcb8008acd3fd0f1fa14f
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-07-31 18:37:52 +0900

    aa

--- day_2024_07_30/InterfaceMain.java
+++ day_2024_07_30/InterfaceMain.java
@@ -2,24 +2,27 @@ package day_2024_07_31;
 
 //	데이터 멤버(상수와 함수선언(abstract) = 추상메서드
 interface Printable {
-	public static final int COUNT = 0;
-	public abstract void print(String doc);
+
+    public static final int COUNT = 0;
+
+    public abstract void print(String doc);
 }
 // interface = abstract = 자식이 구현하라.
+
 class Printer implements Printable {
 
-	@Override
-	public void print(String doc) {
-		System.out.println(doc);
-	}
+    @Override
+    public void print(String doc) {
+        System.out.println(doc);
+    }
 }
 
 public class InterfaceMain {
 
-	public static void main(String[] args) {
-		Printable prn = new Printer();
-		prn.print("안녕하세요");
+    public static void main(String[] args) {
+        Printable prn = new Printer();
+        prn.print("안녕하세요");
 
-	}
+    }
 
 }
