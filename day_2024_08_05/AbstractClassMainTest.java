561e8e15006b0d6907f68a2f90a351add4504fb7
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-05 10:55:37 +0900

    aa

--- day_2024_08_05/AbstractClassMainTest.java
+++ day_2024_08_05/AbstractClassMainTest.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class AbstractClassMainTest {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		final int COUNT = 8;
-		double[] arrGrade = new double[COUNT];
+        final int COUNT = 8;
+        double[] arrGrade = new double[COUNT];
 
-		System.out.println("8학기 학점을 순서대로 입력하세요");
-		double sum = 0;
+        System.out.println("8학기 학점을 순서대로 입력하세요");
+        double sum = 0;
 
-		for (int i = 0; i < arrGrade.length; i++) {
-			System.out.println((i + 1) + "학기 학점→");
-			Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arrGrade.length; i++) {
+            System.out.println((i + 1) + "학기 학점→");
+            Scanner sc = new Scanner(System.in);
 
-			try {
-				arrGrade[i] = sc.nextDouble();
-				sum += arrGrade[i];
+            try {
+                arrGrade[i] = sc.nextDouble();
+                sum += arrGrade[i];
 
-			} catch (Exception e) {
-				System.out.println(" 잘못된 입력입니다. 다시 입력하세요.");
-				i--;
-				continue;
-			}
-		}
-		System.out.println("총점" + sum + "평균" + sum / arrGrade.length);
-	}
+            } catch (Exception e) {
+                System.out.println(" 잘못된 입력입니다. 다시 입력하세요.");
+                i--;
+                continue;
+            }
+        }
+        System.out.println("총점" + sum + "평균" + sum / arrGrade.length);
+    }
 
 }
