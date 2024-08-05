f7d94278017d940df0220f50e693cba2903fec91
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-05 11:35:51 +0900

    aa

--- day_2024_08_05/BoxMain.java
+++ day_2024_08_05/BoxMain.java
@@ -22,10 +22,48 @@ class Box {
         return ob;
     }
 }
+class AppleBox {
+    private Apple ap;
+
+    public void set(Apple a) {
+        ap = a;
+    }
+
+    public Apple get() {
+        return ap;
+    }
+}
+
+
+class OrangeBox {
+    private Orange or;
+
+    public void set(Orange o) {
+        or = o;
+    }
+
+    public Orange get() {
+        return or;
+    }
+}
 
 public class BoxMain {
 
     public static void main(String[] args) {
+        // AppleBox aBox = new AppleBox();
+        // OrangeBox oBox = new OrangeBox();
+
+
+        // aBox.set(new Apple());
+        // oBox.set(new Orange());
+
+
+        // Apple ap = aBox.get();
+        // Orange og = oBox.get();
+
+        // System.out.println(ap);
+        // System.out.println(og);
+
         Box abox = new Box();
         Box obox = new Box();
 
