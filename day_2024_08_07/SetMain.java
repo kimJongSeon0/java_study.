96e78d91856cfc6995ee3fe72d57959e31a4bd18
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-07 16:40:02 +0900

    aa

--- day_2024_08_07/SetMain.java
+++ day_2024_08_07/SetMain.java
@@ -1,11 +1,20 @@
 package day_2024_08_07;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetMain {
 
     public static void main(String[] args) {
+        Set<Num> n1 = new HashSet<>();
+
+        n1.add(new Num(1));
+        n1.add(new Num(2));
+        n1.add(new Num(2));
+
+        System.out.println(n1.size());
+
         Set<Integer> lottoSet = new HashSet<>();
         final int LOTTO = 6;
 
@@ -20,20 +29,23 @@ public class SetMain {
             System.out.println(s);
         }
 
-//      Set<Integer> set = new HashSet<>();
-//      set.add(1);
-//      set.add(2);
-//      set.add(3);
-//      set.add(1);
-//
-//      System.out.println("사이즈: " + set.size());
-//
-//      for (Iterator<Integer> itr = set.iterator(); itr.hasNext();)
-//         System.out.print(itr.next() + '\t');
-//      System.out.println();
-//
-//      for (Integer s : set)
-//         System.out.print(s + '\t');
-//      System.out.println();
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        System.out.println("사이즈: " + set.size());
+
+        for (Iterator<Integer> itr = set.iterator(); itr.hasNext();) {
+            System.out.print(itr.next() + '\t');
+        }
+        System.out.println();
+
+        for (Integer s : set) {
+            System.out.print(s + '\t');
+        }
+        System.out.println();
+
     }
 }
