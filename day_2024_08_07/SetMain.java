221df5d9373d0ea4c4db06eef6a1b686be28b23f
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-07 14:21:12 +0900

    aa

---
+++ day_2024_08_07/SetMain.java
@@ -0,0 +1,39 @@
+package day_2024_08_07;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMain {
+
+    public static void main(String[] args) {
+        Set<Integer> lottoSet = new HashSet<>();
+        final int LOTTO = 6;
+
+        while (lottoSet.size() < LOTTO) {
+            int num = (int) ((Math.random() * 45) + 1);
+            lottoSet.add(num);
+        }
+
+        System.out.println(lottoSet);
+
+        for (Integer s : lottoSet) {
+            System.out.println(s);
+        }
+
+//      Set<Integer> set = new HashSet<>();
+//      set.add(1);
+//      set.add(2);
+//      set.add(3);
+//      set.add(1);
+//
+//      System.out.println("사이즈: " + set.size());
+//
+//      for (Iterator<Integer> itr = set.iterator(); itr.hasNext();)
+//         System.out.print(itr.next() + '\t');
+//      System.out.println();
+//
+//      for (Integer s : set)
+//         System.out.print(s + '\t');
+//      System.out.println();
+    }
+}
