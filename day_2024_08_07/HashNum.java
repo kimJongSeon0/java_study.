ce1b7b50800efcc81b627a2150acf507253bf57a
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-07 18:15:06 +0900

    aa

---
+++ day_2024_08_07/HashNum.java
@@ -0,0 +1,50 @@
+package day_2024_08_07;
+
+import java.util.HashSet;
+
+class Num {
+
+    int num;
+
+    Num(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(num);
+    }
+
+    @Override
+    public int hashCode() {
+        return num % 2;  //출력 순서를 맞추려면 이렇게 변경하면 된다는것까지 알았는데 정확히 왜인지는 모르겠다.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.num == ((Num) obj).num) {
+            return true; 
+        }else {
+            return false;
+        }
+    }
+}
+
+public class HashNum {
+
+    public static void main(String[] args) {
+
+        HashSet<Num> set = new HashSet<>();
+        set.add(new Num(7799));
+        set.add(new Num(9955));
+        set.add(new Num(7799));
+
+        System.out.println("인스턴스 수: " + set.size());
+
+        for (Num n : set) {
+            System.out.print(n.toString() + '\t');
+        }
+
+        System.out.println();
+    }
+}
--- day_2024_08_07/SetMain.java
+++ day_2024_08_07/SetMain.java
@@ -3,56 +3,69 @@ package day_2024_08_07;
 import java.util.HashSet;
 import java.util.Set;
 
-class Num{
-    private int num;
+class Num1 {
 
-    public Num(int n){
-        num = n;
+    int num;
+
+    Num1(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(num);
+    }
+
+    @Override
+    public int hashCode() {
+        return num % 2;  //출력 순서를 맞추려면 이렇게 변경하면 된다는것까지 알았는데 정확히 왜인지는 모르겠다.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.num == ((Num) obj).num) {
+            return true; 
+        }else {
+            return false;
+        }
     }
 }
+
 
 public class SetMain {
 
     public static void main(String[] args) {
-        Set<Num> n1 = new HashSet<>();
+        Set<Num1> n1 = new HashSet<>();
 
-        n1.add(new Num(1));
-        n1.add(new Num(2));
-        n1.add(new Num(3));
+        n1.add(new Num1(1));
+        n1.add(new Num1(2));
+        n1.add(new Num1(3));
 
         System.out.println(n1.size());
 
         // Set<Integer> lottoSet = new HashSet<>();
         // final int LOTTO = 6;
-
         // while (lottoSet.size() < LOTTO) {
         //     int num = (int) ((Math.random() * 45) + 1);
         //     lottoSet.add(num);
         // }
-
         // System.out.println(lottoSet);
-
         // for (Integer s : lottoSet) {
         //     System.out.println(s);
         // }
-
         // Set<Integer> set = new HashSet<>();
         // set.add(1);
         // set.add(2);
         // set.add(3);
         // set.add(1);
-
         // System.out.println("사이즈: " + set.size());
-
         // for (Iterator<Integer> itr = set.iterator(); itr.hasNext();) {
         //     System.out.print(itr.next() + '\t');
         // }
         // System.out.println();
-
         // for (Integer s : set) {
         //     System.out.print(s + '\t');
         // }
         // System.out.println();
-
     }
 }
