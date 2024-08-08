7333e5aff81aadc7d923e2e68e7a14cbff957ad9
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-08 10:13:54 +0900

    aa

--- day_2024_08_07/SetMain.java
+++ day_2024_08_07/SetMain.java
@@ -3,8 +3,6 @@ package day_2024_08_07;
 import java.util.HashSet;
 import java.util.Set;
 
-import day_2024_08_08.Num;
-
 class Num1 {
 
     int num;
@@ -20,12 +18,12 @@ class Num1 {
 
     @Override
     public int hashCode() {
-        return num % 2;  //출력 순서를 맞추려면 이렇게 변경하면 된다는것까지 알았는데 정확히 왜인지는 모르겠다.
+        return num % 3;  //출력 순서를 맞추려면 이렇게 변경하면 된다는것까지 알았는데 정확히 왜인지는 모르겠다.
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this.num == ((Num) obj).num) {
+        if (num == ((Num) obj).num) {
             return true; 
         }else {
             return false;
@@ -38,24 +36,25 @@ public class SetMain {
 
     public static void main(String[] args) {
         
-        // Set<Num1> n1 = new HashSet<>();
+        Set<Num1> n1 = new HashSet<>();
 
-        // n1.add(new Num1(1));
-        // n1.add(new Num1(2));
-        // n1.add(new Num1(3));
+        n1.add(new Num1(1));
+        n1.add(new Num1(2));
+        n1.add(new Num1(2));
 
-        // System.out.println(n1.size());
+        System.out.println(n1.size());
 
-        Set<Integer> lottoSet = new HashSet<>();
-        final int LOTTO = 6;
-        while (lottoSet.size() < LOTTO) {
-            int num = (int) ((Math.random() * 45) + 1);
-            lottoSet.add(num);
-        }
-        System.out.println(lottoSet);
-        for (Integer s : lottoSet) {
-            System.out.println(s);
-        }
+        // Set<Integer> lottoSet = new HashSet<>();
+        // final int LOTTO = 6;
+        // while (lottoSet.size() < LOTTO) {
+        //     int num = (int) ((Math.random() * 45) + 1);
+        //     lottoSet.add(num);
+        // }
+        // System.out.println(lottoSet);
+        // for (Integer s : lottoSet) {
+        //     System.out.println(s);
+        // }
+
         // Set<Integer> set = new HashSet<>();
         // set.add(1);
         // set.add(2);
