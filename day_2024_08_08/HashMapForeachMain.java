55109c06e196e8005b77922b2f59e7e2c414e800
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-08 14:23:56 +0900

    aa

--- day_2024_08_08/HashMapForeachMain.java
+++ day_2024_08_08/HashMapForeachMain.java
@@ -4,21 +4,22 @@ import java.util.HashMap;
 
 public class HashMapForeachMain {
 
-	public static void main(String[] args) {
-		HashMap<Integer, String> map = new HashMap<>();
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
 
-		// Key-Value 기반 데이터 저장
-		map.put(45, "Brown");	//	객체
-		map.put(37, "James");
-		map.put(23, "Martin");
+        // Key-Value 기반 데이터 저장
+        map.put(45, "Brown");	//	객체
+        map.put(37, "James");
+        map.put(23, "Martin");
 
-		System.out.println("1번: " + map.containsKey(45));
-		System.out.println("2번: " + map.containsKey(60));
-		System.out.println("3번: " + map.containsValue("Martin"));
-		System.out.println("4번: " + map.isEmpty());	//	객체 안에 데이터가 있는지 없는지 확인 하는 것.
-		System.out.println("5번: "+ map.size());
-		
-		System.out.println("23번: " + map.get(23));
+        System.out.println("1번: " + map.containsKey(45));
+        System.out.println("2번: " + map.containsKey(60));
+        System.out.println("3번: " + map.containsValue("Martin"));	//	텍스트로 확인.
+        System.out.println("4번: " + map.isEmpty());	//	객체 안에 데이터가 있는지 없는지 확인 하는 것.
+        System.out.println("5번: " + map.size());
+        System.out.println("6번: " + map);
+
+        System.out.println("23번: " + map.get(23));
 //		
 //		// Key만 담고 있는 컬렉션 인스턴스 생성
 //		Set<Integer> ks = map.keySet();
@@ -38,10 +39,7 @@ public class HashMapForeachMain {
 //					System.out.print(map.get(itr.next()) + '\t');
 //				System.out.println();
 //
-			
 
-		
-
-	}
+    }
 
 }
