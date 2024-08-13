6099eaec791fc37d4cce21806977a21d8dcb1d83
WIN-PIVRU5I97P3\ICT02_11 <whdtjs519@naver.com>
2024-08-13 09:37:46 +0900

    aa

--- day_2024_08_08/SetLotteoMain.java
+++ day_2024_08_08/SetLotteoMain.java
@@ -3,37 +3,39 @@ package day_2024_08_08;
 import java.util.HashSet;
 import java.util.Set;
 
-class Num{
+class Num {
+
     private int num;
-    
-    Num(int num){
+
+    Num(int num) {
         this.num = num;
     }
-    
-    public String toString(){
+
+    public String toString() {
         return String.valueOf(num);
     }
 
-    public int hashCode(){
+    public int hashCode() {
         return num % 2;
     }
- 
-    public boolean equals(Object obj){
-        if (this.num == ((Num) obj).num){
+
+    public boolean equals(Object obj) {
+        if (this.num == ((Num) obj).num) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 }
 
 public class SetLotteoMain {
+
     public static void main(String[] args) {
-        
+
         Set<Integer> lottoSet = new HashSet<>();
         final int LOTTO = 6;
-        while (lottoSet.size() < LOTTO){
-            int num = (int)((Math.random() * 45) + 1);
+        while (lottoSet.size() < LOTTO) {
+            int num = (int) ((Math.random() * 45) + 1);
             lottoSet.add(num);
         }
         System.out.println(lottoSet);
